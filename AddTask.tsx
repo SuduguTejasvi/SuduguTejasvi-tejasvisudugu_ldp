dded1ac1ee01f17509f4e06ac08f8171993743da
SuduguTejasvi <tejasvi.sudugu@zemosolabs.com>
2024-07-11 17:39:25 +0530

    Update AddTask.tsx

--- AddTask.tsx
+++ AddTask.tsx
@@ -20,26 +20,3 @@ function AddTask() {
 	);
 }
 export default AddTask;
-// import React, { useState } from 'react';
-// import Search from './Search';
-
-// function AddTask() {
-// 	const [Todos, UpdateTodos] = useState<string[]>(['Task1', 'Task2']);
-
-// 	const addNew = (task: string) => {
-// 		UpdateTodos([...Todos, task]);
-// 	};
-
-// 	return (
-// 		<div>
-// 			<Search add={addNew} />
-// 			<ul>
-// 				{Todos.map(task => (
-// 					<li key={task}>{task}</li>
-// 				))}
-// 			</ul>
-// 		</div>
-// 	);
-// }
-
-// export default AddTask;
