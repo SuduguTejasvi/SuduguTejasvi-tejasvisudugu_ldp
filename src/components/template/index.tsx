c2606a766b065dfca4b2226a28dbe3a361e0ed43
Tejasvi <sudugutejasvi@gmail.com>
2024-08-02 12:56:25 +0530

    refactored template and theme file

--- src/components/template/index.tsx
+++ src/components/template/index.tsx
@@ -1,12 +1,18 @@
+import { Grid } from "@mui/material";
 import React from "react";
-import { CustomBox } from "./styles";
-import {financeTrackerTemplate} from "../../utils/interface"
-const FinanceTrackerTemplate: React.FC<financeTrackerTemplate> = ({ children }) => {
-    return (
-        <CustomBox>
-            {children}
-        </CustomBox>
-    );
-};
+import Form from "../organisms/Card";
+import { background } from "../../utils/constants";
+import { CustomGrid } from "./index.styles";
 
-export default FinanceTrackerTemplate;
+export default function CardLayout() {
+  return (
+    <CustomGrid container>
+      <Grid item xs={8} style={{ height: '100vh' }}>
+        <img src={background} alt="Background" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      </Grid>
+      <Grid item xs={4} style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Form />
+      </Grid>
+    </CustomGrid>
+  );
+}
